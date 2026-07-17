d37baea845a67a61ba51b2db2071ef9c8f64aab5
Niminem <leon.l.lysak@gmail.com>
2026-07-17 16:26:54 -0400

    updates to blink example

--- examples/blink.nim
+++ examples/blink.nim
@@ -1,13 +1,13 @@
 ## This example is a simple blink program that blinks the built-in LED on the board.
-## Successfully tested with Arduino UNO board
+## This example is successfully tested with Arduino UNO board.
 
 import ../src/narduino
 
-setup:
-    pinMode(LED_BUILTIN, OUTPUT)
+setup: # setup function is called once when the board is powered on
+    pinMode(LED_BUILTIN, OUTPUT) # set built-in LED pin to output (sends HIGH / LOW to pin)
 
-loop:
-    digitalWrite(LED_BUILTIN, HIGH)
-    delay(100)
-    digitalWrite(LED_BUILTIN, LOW)
+loop: # loop function is called repeatedly after setup
+    digitalWrite(LED_BUILTIN, HIGH) # send HIGH to pin (turns on LED)
+    delay(100) # wait 100 milliseconds (1/10th of a second)
+    digitalWrite(LED_BUILTIN, LOW) # send LOW to pin (turns off LED)
     delay(100)
